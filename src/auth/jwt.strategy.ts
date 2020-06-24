54227a40e7f4b822e8f1b0e614e9b2e59db74932
JeanRibac <jeanribac95@gmail.com>
2020-06-24 16:11:08 +0300

    added production configuration, changed variables in codebase

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,8 @@ import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as config from 'config'
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ){
         super({
             jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey:"topSecret51",
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
         })
     }
     async validate(payload: JwtPayload){
