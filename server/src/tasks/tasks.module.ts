23d3fad62f32cf506a89736fc0b854b9c5171fd2
JeanRibac <53911977+JeanRibac@users.noreply.github.com>
2020-08-13 11:43:52 +0300

    Update tasks.module.ts

--- server/src/tasks/tasks.module.ts
+++ server/src/tasks/tasks.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { TaskSchema } from './task.model';
 import { UserSchema } from '../auth/auth.model';
 
